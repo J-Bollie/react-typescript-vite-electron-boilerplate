8b3ab66fb05039f983ef12ac2dcd7a8f274e3eca
Jensen Bollen <jensen@Mac.iconos.be>
2026-03-17 13:13:18 +0100

    Update

--- types/svg.d.ts
+++ types/svg.d.ts
@@ -1,4 +1,4 @@
-declare module "*.svg" {
+declare module "*.svg?react" {
 	import React = require("react");
 	export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
 	const src: string;
